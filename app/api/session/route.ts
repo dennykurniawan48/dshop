5d020c1298e023c233126c54013cca9c4d5082eb
Denny Kurniawan <cipher.denny@gmail.com>
2023-11-09 04:12:47 +0700

    clean up

--- app/api/session/route.ts
+++ app/api/session/route.ts
@@ -1,16 +1,20 @@
 import { getSession } from "next-auth/react"
 
-export async function GET(req: Request){
-    const {searchParams} = new URL(req.url)
+export async function GET(req: Request) {
+    try {
+        const { searchParams } = new URL(req.url)
 
-    const requestForNextAuth = {
-        headers: {
-            cookie: searchParams.get("cookie") ?? undefined,
-        },
+        const requestForNextAuth = {
+            headers: {
+                cookie: searchParams.get("cookie") ?? undefined,
+            },
+        }
+
+        const data = await getSession({ req: requestForNextAuth })
+
+        return Response.json({ data })
+    } catch (err) {
+        return Response.json({ data: null })
     }
-
-    const data = await getSession({req: requestForNextAuth})
-
-    return Response.json({data})
 
 }
