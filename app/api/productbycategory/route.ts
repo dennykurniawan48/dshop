6e3dc22dbe6a143efd50a8d124a4bc460bf8e75a
Denny Kurniawan <cipher.denny@gmail.com>
2023-11-13 20:23:58 +0700

    add image and fix limit page

--- app/api/productbycategory/route.ts
+++ app/api/productbycategory/route.ts
@@ -9,14 +9,14 @@ export async function GET(req: Request) {
     if (category.length == 0) {
         const data = await prisma.$transaction([
             prisma.product.count({ where: { isActive: true } }),
-            prisma.product.findMany({ select: { id: true, name: true, categoryId: true, image1: true, price: true, averageRating: true }, where: { isActive: true }, skip: (page - 1) * limit })
+            prisma.product.findMany({ select: { id: true, name: true, categoryId: true, image1: true, price: true, averageRating: true }, where: { isActive: true }, skip: (page - 1) * limit, take:limit })
         ])
         const response = { total: data[0], products: data[1], currentPage: page, totalPage: Math.ceil(data[0] / limit) }
         return Response.json({ "data": response })
     } else {
         const data = await prisma.$transaction([
             prisma.product.count({ where: { isActive: true, categoryId: category } }),
-            prisma.product.findMany({ select: { id: true, name: true, categoryId: true, image1: true, price: true, averageRating: true }, where: { isActive: true, categoryId: category }, skip: (page - 1) * limit })
+            prisma.product.findMany({ select: { id: true, name: true, categoryId: true, image1: true, price: true, averageRating: true }, where: { isActive: true, categoryId: category }, skip: (page - 1) * limit, take:limit})
         ])
         const response = { total: data[0], products: data[1], currentPage: page, totalPage: Math.ceil(data[0] / limit) }
         return Response.json({ "data": response })
